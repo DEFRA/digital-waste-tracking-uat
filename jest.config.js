af00dd9f6aae20cc4f6abbc7187d90cfdd753d56
Lewis Magri <Lewis@limitless.limits.ltd>
2025-05-23 16:43:10 +0100

    More fixes

--- jest.config.js
+++ jest.config.js
@@ -15,11 +15,23 @@ export default {
     '!test/setup/**/*.js'
   ],
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'clover'],
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
   verbose: true,
   transformIgnorePatterns: [],
   maxWorkers: '50%', // Use 50% of available CPU cores for parallel execution
   testTimeout: 30000, // 30 seconds timeout for each test
   maxConcurrency: 5, // Limit concurrent tests to prevent API rate limiting
-  workerIdleMemoryLimit: '512MB' // Restart workers after they use too much memory
+  workerIdleMemoryLimit: '512MB', // Restart workers after they use too much memory
+  bail: false, // Don't stop on first test failure
+  errorOnDeprecated: true, // Make deprecated calls throw errors
+  detectOpenHandles: true, // Detect handles that prevent Jest from exiting
+  forceExit: false // Don't force exit after tests complete
 }
--- test/setup/jestSetupFilesAfterEnv.js
+++ test/setup/jestSetupFilesAfterEnv.js
@@ -1,5 +1,15 @@
 // jest.setup.js
 import { ApiFactory } from '../apis/api-factory.js'
+import { setGlobalDispatcher, Agent } from 'undici'
+
+// If out of function will run as beforeAll:
+// Configure undici with connection pooling and timeout settings
+const agent = new Agent({
+  connections: 10,
+  pipelining: 0,
+  timeout: 30000
+})
+setGlobalDispatcher(agent)
 
 beforeEach(() => {
   globalThis.apis = ApiFactory.create() // Fresh instance per test
@@ -7,4 +17,9 @@ beforeEach(() => {
 
 afterEach(() => {
   delete globalThis.apis // Clean up after test
+})
+
+// Close all connections after all tests complete
+afterAll(async () => {
+  await agent.close()
 })
