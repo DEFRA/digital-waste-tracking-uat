e8b77a8d8fa92e0f7d0681049fab0091f8376c4a
Lewis Magri <lewis@limitless.limits.limited>
2025-12-19 14:18:45 +0000

    DWT-1285: Added tests for the EWC code endpoint's new response structure (#90)
    * DWT-1285: Added tests for the EWC code endpoint's new response structure
    
    * Condenced two tests together
    
    ---------
    
    Co-authored-by: Lewis Magri <Lewis@limitless.limits.ltd>

--- test/specs/WasteRecievingAPI/ReferenceData/get-ewc-codes.test.js
+++ test/specs/WasteRecievingAPI/ReferenceData/get-ewc-codes.test.js
@@ -5,6 +5,7 @@ import { addAllureLink } from '../../../support/helpers/allure-api-logger.js'
 describe('Retrieve EWC Codes', () => {
   beforeEach(async () => {
     await addAllureLink('/DWT-717', 'DWT-717', 'jira')
+    await addAllureLink('/DWT-1285', 'DWT-1285', 'jira')
     // Authenticate and set the auth token
     await authenticateAndSetToken(
       globalThis.testConfig.cognitoClientId,
@@ -12,25 +13,26 @@ describe('Retrieve EWC Codes', () => {
     )
   })
 
-  describe('Retrieve EWC Codes', () => {
-    describe('Get ewc codes list', () => {
-      it('should be able to retrieve valid ewc codes list with isHazardous indicator @allure.label.tag:DWT-717', async () => {
-        const response =
-          await globalThis.apis.wasteMovementExternalAPI.retrieveReferenceData(
-            'ewc-codes'
-          )
-        expect(response.statusCode).toBe(200)
-        expect(response.json.length).toEqual(842)
-        // disabling this for now , should be re-enabled after 1285 is merged
-        //   expect(response.json).toContainEqual({
-        //     code: '200138',
-        //     isHazardous: 'No',
-        //     entryTypeDesc: 'Mirror non-hazardous',
-        //     chapter:
-        //       'Municipal wastes (household waste and similar commercial, industrial and institutional wastes) including separately collected fractions',
-        //     subChapter: 'Separately collected fractions (except 15 01)',
-        //     description: 'Wood other than that mentioned in 20 01 37'
-        //   })
+  describe('Get EWC Codes List', () => {
+    it('should return the full list of EWC codes with all required fields @allure.label.tag:DWT-717 @allure.label.tag:DWT-1285', async () => {
+      const response =
+        await globalThis.apis.wasteMovementExternalAPI.retrieveReferenceData(
+          'ewc-codes'
+        )
+      expect(response.statusCode).toBe(200)
+      expect(response.json.length).toEqual(842)
+
+      const nonHazardousCode = response.json.find(
+        (item) => item.code === '200138'
+      )
+      expect(nonHazardousCode).toEqual({
+        code: '200138',
+        isHazardous: false,
+        entryTypeDesc: 'Mirror non-hazardous',
+        chapter:
+          'Municipal wastes (household waste and similar commercial, industrial and institutional wastes) including separately collected fractions',
+        subChapter: 'Separately collected fractions (except 15 01)',
+        description: 'Wood other than that mentioned in 20 01 37'
       })
     })
   })
