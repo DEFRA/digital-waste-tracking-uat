2b3807254166584e037a958eea6b68cc9e96308f
Padmaja <padmaja.gandham@equalexperts.com>
2025-12-23 09:47:30 +0000

    DWT-1287: modified auto test to check for description

--- test/specs/WasteRecievingAPI/ReferenceData/get-container-types.test.js
+++ test/specs/WasteRecievingAPI/ReferenceData/get-container-types.test.js
@@ -9,6 +9,7 @@ import { addAllureLink } from '~/test/support/helpers/allure-api-logger.js'
 describe('Retrieve Waste Container Types', () => {
   beforeEach(async () => {
     await addAllureLink('/DWT-721', 'DWT-721', 'jira')
+    await addAllureLink('/DWT-1287', 'DWT-1287', 'jira')
     // Authenticate and set the auth token
     await authenticateAndSetToken(
       globalThis.testConfig.cognitoClientId,
@@ -17,7 +18,7 @@ describe('Retrieve Waste Container Types', () => {
   })
 
   describe('Get Waste Container Types list', () => {
-    it('should be able to retrieve valid container types list @allure.label.tag:DWT-721', async () => {
+    it('should be able to retrieve valid container types list @allure.label.tag:DWT-721 @allure.label.tag:DWT-1287', async () => {
       const expectedCodes = [
         'BAG',
         'BAL',
@@ -43,6 +44,12 @@ describe('Retrieve Waste Container Types', () => {
       expect(response.statusCode).toBe(200)
       const codes = response.json.map((item) => item.code)
       expect(codes).toEqual(expectedCodes)
+      // DWT-1287 -adding addtional assertion to verify the structure of the object
+      const codeObj = response.json.find((item) => item.code === 'BOX')
+      expect(codeObj).toEqual({
+        code: 'BOX',
+        description: 'Box / Carton / Crate'
+      })
     })
   })
 })
