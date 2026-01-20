d85ddb01ae9f667f603e993cec8262007144010b
Lewis Magri <Lewis@limitless.limits.ltd>
2026-01-20 10:30:22 +0000

    Added missing files

---
+++ test/specs/WasteRecievingAPI/CreateWasteMovement/waste-item-number-of-containers.test.js
@@ -0,0 +1,77 @@
+import { describe, it, expect, beforeEach } from '@jest/globals'
+import { generateBaseWasteReceiptData } from '../../../support/test-data-manager.js'
+import { authenticateAndSetToken } from '../../../support/helpers/auth.js'
+import { addAllureLink } from '../../../support/helpers/allure-api-logger.js'
+
+describe('Number of containers for a waste item Validation', () => {
+  let wasteReceiptData
+
+  beforeEach(async () => {
+    await addAllureLink('/DWT-337', 'DWT-337', 'jira')
+    wasteReceiptData = generateBaseWasteReceiptData()
+
+    // Authenticate and set the auth token
+    await authenticateAndSetToken(
+      globalThis.testConfig.cognitoClientId,
+      globalThis.testConfig.cognitoClientSecret
+    )
+  })
+
+  describe('Number of waste containers must be a positive integer or a 0', () => {
+    it(
+      'should accept a waste with number of containers greater than 0 ' +
+        ' @allure.label.tag:DWT-337',
+      async () => {
+        wasteReceiptData.wasteItems[0].numberOfContainers = 5
+        const response =
+          await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
+            wasteReceiptData
+          )
+
+        expect(response.statusCode).toBe(201)
+        expect(response.json).toHaveProperty('wasteTrackingId')
+        // ToDo:Assert that the record has been created successfully in the DB
+      }
+    )
+    it(
+      'should accept a waste when number of containers supplied value is0 ' +
+        ' @allure.label.tag:DWT-337',
+      async () => {
+        wasteReceiptData.wasteItems[0].numberOfContainers = 0
+        const response =
+          await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
+            wasteReceiptData
+          )
+
+        expect(response.statusCode).toBe(201)
+        expect(response.json).toHaveProperty('wasteTrackingId')
+        // ToDo:Assert that the record has been created successfully in the DB
+      }
+    )
+  })
+
+  it(
+    'should not allow waste movement to be created when number of containers is not provided for a waste item' +
+      ' @allure.label.tag:DWT-337',
+    async () => {
+      delete wasteReceiptData.wasteItems[0].numberOfContainers
+
+      const response =
+        await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
+          wasteReceiptData
+        )
+      expect(response.statusCode).toBe(400)
+      expect(response.json).toEqual({
+        validation: {
+          errors: [
+            {
+              key: 'wasteItems.0.numberOfContainers',
+              errorType: 'NotProvided',
+              message: '"wasteItems[0].numberOfContainers" is required'
+            }
+          ]
+        }
+      })
+    }
+  )
+})
