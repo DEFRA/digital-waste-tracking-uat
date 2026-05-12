32133162df51ffbf79c5584d17709dd8d989380e
Lewis Magri <Lewis@limitless.limits.ltd>
2026-05-12 11:09:21 +0100

    DWTA-189: Added test for extra SEPA arrangement section for reciever auth numbers, and refactored some of the test naming

--- test/specs/WasteMovementExternalAPI/CreateWasteMovement/receiver-authorisation-number.test.js
+++ test/specs/WasteMovementExternalAPI/CreateWasteMovement/receiver-authorisation-number.test.js
@@ -3,11 +3,10 @@ import { generateBaseWasteReceiptData } from '../../../support/test-data-manager
 import { authenticateAndSetToken } from '../../../support/helpers/auth.js'
 import { addAllureLink } from '../../../support/helpers/allure-api-logger.js'
 
-describe('Site Waste Authorisation Validation', () => {
+describe('Receiver Authorisation Number Validation', () => {
   let wasteReceiptData
 
   beforeEach(async () => {
-    await addAllureLink('/DWT-339', 'DWT-339', 'jira')
     wasteReceiptData = generateBaseWasteReceiptData()
 
     // Authenticate and set the auth token
@@ -17,11 +16,12 @@ describe('Site Waste Authorisation Validation', () => {
     )
   })
 
-  describe('Valid Site Waste Authorisation Numbers', () => {
+  describe('Valid Receiver Authorisation Numbers', () => {
     it(
-      'should accept waste movement with single valid site waste authorisation number' +
+      'should accept waste movement when receiver authorisation number uses an accepted standard licence format' +
         ' @allure.label.tag:DWT-339',
       async () => {
+        await addAllureLink('/DWT-339', 'DWT-339', 'jira')
         wasteReceiptData.receiver.authorisationNumber = 'PPC/A/9999999'
         const response =
           await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
@@ -33,9 +33,10 @@ describe('Site Waste Authorisation Validation', () => {
     )
 
     it(
-      'should accept waste movement with valid authorisation format for England or Wales' +
+      'should accept waste movement when receiver authorisation number is valid for England or Wales' +
         ' @allure.label.tag:DWT-339',
       async () => {
+        await addAllureLink('/DWT-339', 'DWT-339', 'jira')
         wasteReceiptData.receiver.authorisationNumber = 'XX9999XX'
         const response =
           await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
@@ -47,9 +48,10 @@ describe('Site Waste Authorisation Validation', () => {
     )
 
     it(
-      'should accept waste movement with a valid authorisation format for Scotland' +
+      'should accept waste movement when receiver authorisation number is valid under SEPA PPC arrangements' +
         ' @allure.label.tag:DWT-339',
       async () => {
+        await addAllureLink('/DWT-339', 'DWT-339', 'jira')
         wasteReceiptData.receiver.authorisationNumber = 'PPC/A/SEPA9999-9999'
         const response =
           await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
@@ -61,10 +63,26 @@ describe('Site Waste Authorisation Validation', () => {
     )
 
     it(
-      'should accept waste movement with a valid authorisation format for Northern Ireland' +
+      'should accept waste movement when receiver authorisation number is valid for Northern Ireland' +
         ' @allure.label.tag:DWT-339',
       async () => {
+        await addAllureLink('/DWT-339', 'DWT-339', 'jira')
         wasteReceiptData.receiver.authorisationNumber = 'WPPC 99/99'
+        const response =
+          await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
+            wasteReceiptData
+          )
+        expect(response.statusCode).toBe(201)
+        expect(response.json).toHaveProperty('wasteTrackingId')
+      }
+    )
+
+    it(
+      'should accept waste movement when receiver authorisation number includes an additional activity segment for SEPA receiver sites' +
+        ' @allure.label.tag:DWTA-189',
+      async () => {
+        await addAllureLink('/DWTA-189', 'DWTA-189', 'jira')
+        wasteReceiptData.receiver.authorisationNumber = 'WML/L/9999999/99'
         const response =
           await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
             wasteReceiptData
@@ -75,11 +93,12 @@ describe('Site Waste Authorisation Validation', () => {
     )
   })
 
-  describe('Invalid Site Waste Authorisation Scenarios', () => {
+  describe('Invalid Receiver Authorisation Number Scenarios', () => {
     it(
-      'should reject waste movement when authorisation number is empty string' +
+      'should reject waste movement when receiver authorisation number is blank' +
         ' @allure.label.tag:DWT-339',
       async () => {
+        await addAllureLink('/DWT-339', 'DWT-339', 'jira')
         wasteReceiptData.receiver.authorisationNumber = ''
         const response =
           await globalThis.apis.wasteMovementExternalAPI.receiveMovement(
@@ -101,7 +120,7 @@ describe('Site Waste Authorisation Validation', () => {
       }
     )
     it(
-      'should reject waste movement when authorisation number is not in the correct format' +
+      'should reject waste movement when receiver authorisation number is not a valid UK site authorisation number' +
         ' @allure.label.tag:DWT-578',
       async () => {
         await addAllureLink('/DWT-578', 'DWT-578', 'jira')
