6c33c609b344bc8c945be599cecfb417732b8806
Lewis Magri <lewis@limitless.limits.limited>
2026-09-22 17:48:33 +0100

    Added additional tests around apiCode for Delivery and Reciept

--- test/specs/WasteMovementExternalAPI/beta-1/api-code.test.js
+++ test/specs/WasteMovementExternalAPI/beta-1/api-code.test.js
@@ -54,5 +54,95 @@ describe('Beta-1 API Code', () => {
         instance: `/beta-1/movements/${movementId}/collection`
       })
     })
+
+    it('should reject recording a delivery when the API code is not recognised', async () => {
+      const createResponse =
+        await globalThis.apis.wasteMovementExternalAPI.beta1.createMovement(
+          beta1.generateBaseMovementData()
+        )
+      expect(createResponse.statusCode).toBe(201)
+      const movementId = createResponse.json.data.movementId
+
+      const collectionResponse =
+        await globalThis.apis.wasteMovementExternalAPI.beta1.createCollection(
+          movementId,
+          beta1.generateBaseCollectionData()
+        )
+      expect(collectionResponse.statusCode).toBe(201)
+
+      const deliveryData = beta1.generateBaseDeliveryData([movementId])
+      deliveryData.apiCode = randomUUID()
+
+      const response =
+        await globalThis.apis.wasteMovementExternalAPI.beta1.createDelivery(
+          deliveryData
+        )
+
+      expect(response.statusCode).toBe(400)
+      expect(response.json).toEqual({
+        type: 'https://waste-tracking.service.gov.uk/problems/bad-request',
+        title: 'Bad Request',
+        detail: 'the API Code supplied is invalid',
+        instance: '/beta-1/deliveries'
+      })
+    })
+
+    it('should reject recording a receipt against a delivery when the API code is not recognised', async () => {
+      const createResponse =
+        await globalThis.apis.wasteMovementExternalAPI.beta1.createMovement(
+          beta1.generateBaseMovementData()
+        )
+      expect(createResponse.statusCode).toBe(201)
+      const movementId = createResponse.json.data.movementId
+
+      const collectionResponse =
+        await globalThis.apis.wasteMovementExternalAPI.beta1.createCollection(
+          movementId,
+          beta1.generateBaseCollectionData()
+        )
+      expect(collectionResponse.statusCode).toBe(201)
+
+      const deliveryResponse =
+        await globalThis.apis.wasteMovementExternalAPI.beta1.createDelivery(
+          beta1.generateBaseDeliveryData([movementId])
+        )
+      expect(deliveryResponse.statusCode).toBe(201)
+      const deliveryId = deliveryResponse.json.data.deliveries[0].deliveryId
+
+      const receiptData = beta1.generateBaseReceiptData()
+      receiptData.apiCode = randomUUID()
+
+      const response =
+        await globalThis.apis.wasteMovementExternalAPI.beta1.createReceiptWithDeliveryId(
+          deliveryId,
+          receiptData
+        )
+
+      expect(response.statusCode).toBe(400)
+      expect(response.json).toEqual({
+        type: 'https://waste-tracking.service.gov.uk/problems/bad-request',
+        title: 'Bad Request',
+        detail: 'the API Code supplied is invalid',
+        instance: `/beta-1/deliveries/${deliveryId}/receipt`
+      })
+    })
+
+    it('should reject recording a receipt without a delivery ID when the API code is not recognised', async () => {
+      const receiptData = beta1.generateBaseReceiptWithoutDeliveryIdData()
+      receiptData.apiCode = randomUUID()
+
+      const response =
+        await globalThis.apis.wasteMovementExternalAPI.beta1.createReceiptWithoutDeliveryId(
+          receiptData
+        )
+
+      expect(response.statusCode).toBe(400)
+      expect(response.json).toEqual({
+        type: 'https://waste-tracking.service.gov.uk/problems/bad-request',
+        title: 'Bad Request',
+        detail: 'the API Code supplied is invalid',
+        instance: '/beta-1/receipts'
+      })
+    })
   })
 })
